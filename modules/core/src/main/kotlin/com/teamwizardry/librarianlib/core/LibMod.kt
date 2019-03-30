3df0af0be426376e23cec8beff839b81fd8d453f
Pierce Corcoran <pierce@plasticcow.com>
2019-03-30 14:06:41 -0700

    Init commit

---
+++ modules/core/src/main/kotlin/com/teamwizardry/librarianlib/core/LibMod.kt
@@ -0,0 +1,9 @@
+package com.teamwizardry.librarianlib.core
+
+import net.fabricmc.api.ModInitializer
+
+object LibMod: ModInitializer {
+    override fun onInitialize() {
+        println("LibrarianLib Core: Initialized")
+    }
+}
---
+++ modules/utils/src/main/kotlin/com/teamwizardry/librarianlib/utils/LibMod.kt
@@ -0,0 +1,9 @@
+package com.teamwizardry.librarianlib.utils
+
+import net.fabricmc.api.ModInitializer
+
+object LibMod: ModInitializer {
+    override fun onInitialize() {
+        println("LibrarianLib Utils: Initialized")
+    }
+}
